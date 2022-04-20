551517416d48891c4ddbc313262f9bdc515d9e32
Andrea Santos <andreasantos@macbook-pro.myfiosgateway.com>
2022-04-20 17:08:57 -0400

    city and category search boxes on home html

--- src/main/java/NewInTown/controller/NewInTownController.java
+++ src/main/java/NewInTown/controller/NewInTownController.java
@@ -5,7 +5,9 @@ import NewInTown.model.RestaurantData;
 import NewInTown.service.restaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -26,19 +28,25 @@ public class NewInTownController {
 
 
 
-        //creates a list of all businesses
-        List<Businesses> businessList = restaurantDataTest.getBusinesses();
-
-        //loops through each business object
-        for (int i = 0; i < businessList.size(); i++) {
-            //loops through category after each business
-            for(int x = 0; x < businessList.get(i).getCategories().size(); x++) {
-                System.out.println(businessList.get(i).getCategories().get(x).getTitle());
-            }
-
-        }
-
+//        //creates a list of all businesses
+//        List<Businesses> businessList = restaurantDataTest.getBusinesses();
+//
+//        //loops through each business object
+//        for (int i = 0; i < businessList.size(); i++) {
+//            //loops through category after each business
+//            for(int x = 0; x < businessList.get(i).getCategories().size(); x++) {
+//                System.out.println(businessList.get(i).getCategories().get(x).getTitle());
+//            }
+//        }
         return "home";
+    }
+
+    @RequestMapping("/search")
+    public String searchResult (@RequestParam("userEnteredCity") String city, @RequestParam("userEnteredCategory") String category,
+                                ModelMap modelMap) throws IOException {
+        RestaurantData userInput = newRestaurantService.fetchRestaurantInfo(city, category);
+        modelMap.put("userInputKey", userInput);
+        return "search";
     }
 //consider try catch block when calling service to handle errors
 }
